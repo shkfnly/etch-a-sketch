2d2a56e7ba2a163f51895ddadf2ef00bdd6ff04c
Ashoka Finley <ashoka.finley@gmail.com>
2014-09-27 20:02:24 -0700

    Created feature to allow for implementation of user selected grid sizes

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,22 @@
 
 $(document).ready(function(){
   //While loop including scopes to create container div rows
+  function grid(size){
   var i = 0;
-  var rows = 17
-  var columns = 17 
+  var rows = parseInt(size) + 1;
+  var columns = parseInt(size) + 1;
+  console.log(rows);
+  console.log(columns);
+  console.log(size);
+  var height = 800 / (size);
+  var width = 800 / (size);
   while(++i < rows){
     (function(i){
         $('body').append("<div class='containers row" + i + "'></div>");
     })(i);
   }
   //Double while loop containing scopes to create columns
-  $('.containers').css({'width': '800px', 'height': '50px'})
+  $('.containers').css({'width': '800px', 'height': height})
   var j = 0;
   while(++j < rows){
     (function(j){
@@ -23,6 +29,7 @@ $(document).ready(function(){
   })(j);
   }
   $('div > div').addClass('colorsOff');
+  $('div > div').css({'width': width, 'height': height})
   $('div > div').hover(
     function(){
        $(this).addClass('colorsOn');
@@ -39,9 +46,12 @@ $(document).ready(function(){
   $('button').click(function(){
     $('div > div').removeClass('colorsOn');
     $('div > div').removeClass('colorsOn2');
-    alert("Hey");
-
+    var newsize = prompt("How large would you like grid?");
+    console.log(newsize);
+    $('body').html('')
+    grid(newsize);
   })
-
+  };
+  grid(16);
 
 });
