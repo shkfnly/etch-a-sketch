5b87dfa16e6d25ec46f1d5d7474f7aa7f43a05d0
Ashoka Finley <ashoka.finley@gmail.com>
2014-09-27 19:25:32 -0700

    added hover effect and click erase

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,26 +2,46 @@
 $(document).ready(function(){
   //While loop including scopes to create container div rows
   var i = 0;
-  while(++i < 5){
+  var rows = 17
+  var columns = 17 
+  while(++i < rows){
     (function(i){
         $('body').append("<div class='containers row" + i + "'></div>");
     })(i);
   }
   //Double while loop containing scopes to create columns
-  $('.containers').css({'width': '800px', 'height': '200px'})
+  $('.containers').css({'width': '800px', 'height': '50px'})
   var j = 0;
-  while(++j < 5){
+  while(++j < rows){
     (function(j){
       var k = 0;
-      while(++k < 5){
+      while(++k < columns){
         (function(k){
         $('.row' + j).append("<div id='r" + j +"c" + k + "'></div>")
       })(k);
       }
   })(j);
   }
-  $('div > div').addClass('colors');
+  $('div > div').addClass('colorsOff');
+  $('div > div').hover(
+    function(){
+       $(this).addClass('colorsOn');
+  }, function(){
+    $(this).addClass('colorsOn2');
+  });
+  $('div > div').click(function(){
+    $('div > div').removeClass('colorsOn');
+    $('div > div').removeClass('colorsOn2')
 
-  
+  })
+  $('body').append('<button>Click To Reset</button');
+  $('button').css({'margin' : '0 auto'})
+  $('button').click(function(){
+    $('div > div').removeClass('colorsOn');
+    $('div > div').removeClass('colorsOn2');
+    alert("Hey");
+
+  })
+
 
 });
